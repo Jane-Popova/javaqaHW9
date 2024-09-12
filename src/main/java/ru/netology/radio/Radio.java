1cb7d5b3ff8c42371bb3323e3de211420ebf371b
Evgenia Popova <jane477@yandex.ru>
2024-09-12 11:40:42 +0300

    first commit

---
+++ src/main/java/ru/netology/radio/Radio.java
@@ -0,0 +1,84 @@
+package ru.netology.radio;
+
+public class Radio {
+    private int currentRadio;
+
+    public int getCurrentRadio() {
+        return currentRadio;
+    }
+
+
+    public void setCurrentRadio(int newCurrentRadio) {
+//        if (newCurrentRadio < 0) {
+//            return;
+//        }
+//        if (newCurrentRadio > 9) {
+//            return;
+//        }
+        currentRadio = newCurrentRadio;
+    }
+
+
+    public void increaseRadio() {
+        if (currentRadio < 9) {
+            currentRadio = currentRadio + 1;
+        }
+        if (currentRadio >= 9) {
+            currentRadio = 0;
+        }
+        setCurrentRadio(currentRadio);
+    }
+
+    public void reduceRadio () {
+        if (currentRadio > 0) {
+            currentRadio = currentRadio - 1;
+        }
+        if (currentRadio <= 0) {
+            currentRadio = 9;
+        }
+        setCurrentRadio(currentRadio);
+    }
+
+
+
+        private int currentVolume;
+
+        public int getCurrentVolume() {
+            return currentVolume;
+        }
+
+        public void setCurrentVolume(int newCurrentVolume) {
+//            if (newCurrentVolume < 0) {
+//                return;
+//            }
+//            if (newCurrentVolume > 100) {
+//                return;
+//            }
+            currentVolume = newCurrentVolume;
+        }
+
+
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;
+        }
+        if (currentVolume >= 100) {
+            currentVolume = 100;
+        }
+        setCurrentVolume(currentVolume);
+    }
+
+        public void reduceVolume () {
+            if (currentVolume > 0) {
+                currentVolume = currentVolume - 1;
+            }
+            if (currentVolume <= 0) {
+                currentVolume = 0;
+            }
+            setCurrentVolume(currentVolume);
+        }
+}
+
+
+
+
---
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -0,0 +1,115 @@
+package ru.netology.radio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class RadioTest {
+
+    @Test
+    public void testIncreasingRadioBy1() {
+        Radio cond = new Radio();
+
+        cond.setCurrentRadio(7);
+
+        cond.increaseRadio();
+        int expected = 8;
+        int actual = cond.getCurrentRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testReducingRadioBy1() {
+        Radio cond = new Radio();
+
+        cond.setCurrentRadio(7);
+
+        cond.reduceRadio();
+        int expected = 6;
+        int actual = cond.getCurrentRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIncreasingMaxRadio() {
+        Radio cond = new Radio();
+
+        cond.setCurrentRadio(9);
+
+        cond.increaseRadio();
+        int expected = 0;
+        int actual = cond.getCurrentRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testReducingMinRadio() {
+        Radio cond = new Radio();
+
+        cond.setCurrentRadio(0);
+
+        cond.reduceRadio();
+        int expected = 9;
+        int actual = cond.getCurrentRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIncreasingVolumeBy1() {
+        Radio cond = new Radio();
+
+        cond.setCurrentVolume(10);
+
+        cond.increaseVolume();
+        int expected = 11;
+        int actual = cond.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testReducingVolumeBy1() {
+        Radio cond = new Radio();
+
+        cond.setCurrentVolume(10);
+
+        cond.reduceVolume();
+        int expected = 9;
+        int actual = cond.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIncreasingMaxVolume() {
+        Radio cond = new Radio();
+
+        cond.setCurrentVolume(100);
+
+        cond.increaseVolume();
+        int expected = 100;
+        int actual = cond.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testReducingMinVolume() {
+        Radio cond = new Radio();
+
+        cond.setCurrentVolume(0);
+
+        cond.reduceVolume();
+        int expected = 0;
+        int actual = cond.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+  
+}
